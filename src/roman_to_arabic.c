6e212553667267306ccbbbfc1d7331b1291bd979
Aleksey Furaev <Fuuura@mail.ru>
2024-02-26 23:58:37 +0300

    changed

--- src/roman_to_arabic.c
+++ src/roman_to_arabic.c
@@ -3,14 +3,16 @@
 
 #define MAX_LENGTH 15
 
-int check(int *I, int *V, int *X, int *L, int *C, int *D, int *M);
+int check(char *str, int *I, int *V, int *X, int *L, int *C, int *D, int *M);
 void fck();
 void fix_previous(int **n);
 
 int main() {
     int error = 0;
     int I = 0, V = 0, X = 0, L = 0, C = 0, D = 0, M = 0;
-    error = check(&I, &V, &X, &L, &C, &D, &M);
+    char str[15];
+    scanf("%s", str);
+    error = check(str, &I, &V, &X, &L, &C, &D, &M);
     if (error) {
         fck();
         return 1;
@@ -20,38 +22,38 @@ int main() {
     return 0;
 }
 
-int check(int *I, int *V, int *X, int *L, int *C, int *D, int *M) {
+int check(char *str, int *I, int *V, int *X, int *L, int *C, int *D, int *M) {
     int error = 0;
-    char ch;
-    while (ch != 10) 
-    {
-        ch = fgetc(stdin);
-        if (ch == 'I') {
+    // char ch;
+    // while (ch != 10)
+    for (int i = 0; str[i]; i++) {
+        // char ch = fgetc(stdin);
+        if (str[i] == 'I') {
             *I = *I + 1;
-        } else if (ch == 'V') {
+        } else if (str[i] == 'V') {
             *V = *V + 1;
             fix_previous(&I);
-        } else if (ch == 'X') {
+        } else if (str[i] == 'X') {
             *X = *X + 1;
             fix_previous(&I);
             fix_previous(&V);
-        } else if (ch == 'L') {
+        } else if (str[i] == 'L') {
             *L = *L + 1;
             fix_previous(&V);
             fix_previous(&X);
-        } else if (ch == 'C') {
+        } else if (str[i] == 'C') {
             *C = *C + 1;
             fix_previous(&X);
             fix_previous(&L);
-        } else if (ch == 'D') {
+        } else if (str[i] == 'D') {
             *D = *D + 1;
             fix_previous(&L);
             fix_previous(&C);
-        } else if (ch == 'M') {
+        } else if (str[i] == 'M') {
             *M = *M + 1;
             fix_previous(&C);
             fix_previous(&D);
-        } else if (ch != 10) {
+        } else {
             error++;
             break;
         }
